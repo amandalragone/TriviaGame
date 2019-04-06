1df445ce90df114d4c36f6646c8703f07ec4356b
Amanda <amandalragone@gmail.com>
2019-04-05 23:17:09 -0500

    Was able to reduce the code by creating one function for every click, and also created the questionStatus function, to display if the user got the answer right or not.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+    var images = ["new-york-city.jpg", "gilmore-girls.jpg", "jensen-ackles.jpg", "aladdin.jpg", "voldemort.jpg", "ring.jpg", "snow-white.jpg", "thor.jpg", "phoebe-ross.jpg", "martin-freeman.jpg"];
+    var answers = ["New York", "A single mother and her daughter.", "Jensen Ackles", "crystal clear", "Tom Marvolo Riddle", "in the darkness bind them.", "Snow White and the Seven Dwarfs", "Thor", "Phoebe and Ross.", "Martin Freeman"];
+
     var seconds = 30;
     var intervalId;
 
@@ -7,18 +10,24 @@ $(document).ready(function() {
     var correctAnswers;
     var incorrectAnswers;
     var unansweredQuestions;
+    var questionCount;
+
+    //To start the game, everything except for the H1 and the Start button will be hidden.
 
     $(".question").hide();
     $("#results").hide();
 
+    //This function will be called whenever the Start button is pressed at the beginning of the game, or when "Start Over" is pressed in the results screen.
+
     function startGame() {
 
+      $("#game").empty();
+
+      questionCount = 0;
       correctAnswers = 0;
       incorrectAnswers = 0;
       unansweredQuestions = 0;
 
-
-      // $("#game").empty();
       $("#time").html("<h2>Time Remaining: " + seconds + " Seconds</h2>");
      
       $(".question").first().show().addClass("question_asked").removeClass("question");
@@ -28,48 +37,118 @@ $(document).ready(function() {
 
     }
 
-    // function 
+    //This function will stop the Interval.
 
-    //   setTimeout(fourSeconds, 1000 * 4);
+    function stop() {
+    
+      clearInterval(intervalId);
+    };
 
-    // }
+    //This function will reset seconds to 30.
+
+    function resetSeconds() {
+      seconds = 30;
+      $("#time").html("<h2>" + seconds + "</h2>");
+      intervalId = setInterval(decrement, 1000);
+      decrement();
+    }
     
-    
+    //This function indicates by how many seconds the Interval will decrease.
+
     function decrement() {
     
         //  Decrease seconds by one.
         seconds--;
     
-        //  Show the second remaining in the #time tag.
+        //  Show the seconds remaining in the #time tag.
         $("#time").html("<h2>Time remaining: " + seconds + " Seconds</h2>");
     
-        //  Once number hits zero...
+        //  If seconds hits zero...
         if (seconds === 0) {
     
-          unansweredQuestions++;
-
-          stop();
-
-          resetSeconds();
+          //...run the questionStatus function.
     
-          $(".question_asked").hide();
-          $(".question").first().show().removeClass("question").addClass("question_asked");
+          questionStatus("unanswered");
 
-          if (correctAnswers + incorrectAnswers + unansweredQuestions === 10) {
-          results();
-      };
         }
       }
+
+      //This function will run whenever the user clicks an answer, or whenever seconds = 0.
+
+      function questionStatus(answerChosen) {
+
+        //Question displayed will be hidden and Interval will stop.
+        questionCount++;
+        $(".question_asked").hide();
+        stop();
+
+        //Function will analyze if an answer was selected and, if so, which one. Then, it will diplay the question status based on that. It will also increase the count of either correct answers, incorrect answers, or unanswered questions.
+
+          if (answerChosen === "correct") {
+
+            var status = `
+            <div>
+            <h3>You're right!</h3>
+            <img src='assets/images/${images[questionCount - 1]}' width=250 height=250>
+            </div>
+            `
+
+            $("#game").html(status);
+            correctAnswers++;
+
+          } else if (answerChosen === "incorrect") {
+
+            var status = `
+            <div>
+            <h3>Nope!</h3>
+            <p>The correct answer was: ${answers[questionCount - 1]}</p>
+            <img src='assets/images/${images[questionCount - 1]}' width=250 height=250>
+            </div>
+            `
+
+            $("#game").html(status);
+            
+            incorrectAnswers++;
+
+          } else {
+
+            var status = `
+            <div>
+            <h3>Time's up!</h3>
+            <p>The correct answer was: ${answers[questionCount - 1]}</p>
+            <img src='assets/images/${images[questionCount - 1]}' width=250 height=250>
+            </div>
+            `
+
+            $("#game").html(status);
+            unansweredQuestions++;
+
+          }
+
+        //After 4 seconds, either next question or results screen will show up.
+
+        setTimeout(function() {
+            
+          $("#game").empty();
+          $(".question").first().show().removeClass("question").addClass("question_asked");
+          resetSeconds();
+  
+            if (correctAnswers + incorrectAnswers + unansweredQuestions === 10) {
+                    
+              results();
+          
+            };
+  
+        }, 4000);
+
+
+      }
     
-      function stop() {
-    
-        clearInterval(intervalId);
-      };
+     //This is the function that shows the results screen after all questions have been displayed to the user.
 
       function results () {
         stop();
 
-        // $("#game").empty();
         $("#time").empty();
         $("#correctAnswers").text("Correct Answers: " + correctAnswers);
         $("#incorrectAnswers").text("Incorrect Answers: " + incorrectAnswers);
@@ -77,65 +156,26 @@ $(document).ready(function() {
         $("#results").show();
       };
 
-      function resetSeconds() {
-        seconds = 30;
-        $("#time").html("<h2>" + seconds + "</h2>");
-        intervalId = setInterval(decrement, 1000);
-        decrement();
-      }
-    
+      
+    //When the start button is clicked, 
       button.onclick = function(event) {
 
-      correctAnswers = 0;
-      incorrectAnswers = 0;
-      unansweredQuestions = 0;
-
-
-      $("#game").empty();
-      $("#time").html("<h2>Time Remaining: " + seconds + " Seconds</h2>");
-      $(".question").first().show().addClass("question_asked").removeClass("question");
-
-      clearInterval(intervalId);
-      resetSeconds();
+      startGame();
         
     };
 
+    $("span").click(function(){
 
-    $(".correct").on("click", function(){
-    
-      correctAnswers++;
+      var spanClass = $(this).attr("class");
+      console.log(spanClass);
+      questionStatus(spanClass);
+      // var correctAnswer = $(this).siblings("span").hasClass("correct");
 
-      stop();
+      // // var correctAnswer = $(this).siblings().hasClass("correct");
+      // console.log(correctAnswer);
 
-      resetSeconds();
- 
+    })
 
-      $(".question_asked").hide();
-      $(".question").first().show().removeClass("question").addClass("question_asked");
-
-
-      if (correctAnswers + incorrectAnswers + unansweredQuestions === 10) {
-        
-        results();
-      };
-    });
-
-    $(".incorrect").on("click", function(){
-     
-      incorrectAnswers++;
-
-      stop();
-      resetSeconds();
-    
-      $(".question_asked").hide();
-      $(".question").first().show().removeClass("question").addClass("question_asked");
-
-      if (correctAnswers + incorrectAnswers + unansweredQuestions === 10) {
-        
-        results();
-      };
-
-    });
 
     $("#startAgain").on("click", function(){
       
